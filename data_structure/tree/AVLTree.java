9437a10b573f4c0a1370f7f9d85a1b8bda44ef01
Febregaz <1844927304@qq.com>
2020-09-27 08:34:20 +0000

    GitPod Test For Push

--- data_structure/tree/AVLTree.java
+++ data_structure/tree/AVLTree.java
@@ -2,7 +2,7 @@ package data_structure.tree;
 
 public class AVLTree {
     public static void main(String[] args){
-        System.out.println("AVL");
+        System.out.println("AVL Test");
     }
 
 }
