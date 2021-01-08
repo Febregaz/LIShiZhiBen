6505f230902cef4cebc13ea4b6e93bd61f4d47fc
Unattached <1844927304@qq.com>
2021-01-08 11:45:25 +0800

    ArrayTool添加初始化字符串数组函数；基数排序代码（未完成）

--- algorithm/sort/ArrayTool.java
+++ algorithm/sort/ArrayTool.java
@@ -9,6 +9,7 @@ package sort;
  V1.0    20201229     LYZ      实现可以公用的数组功能
  V1.1    20210105     LYZ      添加数组初始化函数
  V1.2    20210106     LYZ      添加数组初始化函数中数组的坐标注释
+ V1.3    20210108     LYZ      添加初始化字符串数组函数
  ***************************************************/
 public class ArrayTool {
 
@@ -29,6 +30,11 @@ public class ArrayTool {
     public static int[] ArrayInit(){
                            /*0 1  2  3  4  5  6  7  8  9 10 11*/
         int[] simpleArray = {4,66,42,76,22,5,44,33,67,21,1,2};
+        return simpleArray;
+    }
+
+    public static String[] ArrayInitStr(){
+        String[] simpleArray = {"qd","abc", "qwe","hhh","a","cws", "ope"};
         return simpleArray;
     }
 
